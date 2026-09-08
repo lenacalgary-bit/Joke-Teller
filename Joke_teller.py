0f49f56b3c57671783f90f4c6225243f652af68d
lenacalgary-bit <lenacalgary@gmail.com>
2026-09-08 01:17:52 -0400

    Add files via upload

--- Joke_teller.py
+++ Joke_teller.py
@@ -1,18 +1,6 @@
- # TODO types of jokes: knock-knock, pun, q and a, tongue twisters
+# TODO types of jokes: knock-knock, pun, q and a, tongue twisters
 # FUN write(e.g. "Home", move=False, align='left', font=(fontname, fontsize, fonttype))
-"""
-Q. 3 "Why did cavemen draw pictures of hippopotamuses and rhionceroses on their wall?" A. "Because they couldn't spell the animals\' names."
-Q. 4 "On what nuts can pictures hang on?" A. "Wall-nuts."
-Q. 5 "What happened when 500 hares get loose in the center of town?" A. "The police had to comb the area."
-Q. 6 "What do you call a very popular perfume?" A. "A best-smeller."
-Q. 7 "What do you call a polar bear wearing earmuffs?" A. "Anything you want. He can't hear you!"
-Q. 8 "What happens when a ghost gets lost in the fog?" A. "He is mist."
-Q. 9 "What do you call two spiders that just got married?" A. "Newlywebs."
-Q. 10 "What paces back and forth on the ocean floor?" A. "A nervous wreck."
-Q. 11 "What goes thump, thump, thump, squish, thump, thump, thump, squish?" A. "An elephant with one wet shoe."
-Q. 12 "When is a baseball player like a spider?" A. "When he catches a fly."
-Q. 13 "What kind of a fish goes best with peanut butter?" A. "Jelly-fish."
-Q. """
+
 # This is the start up
 import turtle as t
 import random as r
@@ -36,6 +24,17 @@ Qjokes = [
     "What do you get when you say \"tornado\" ten times forwards and backwards?",#4
     "How are two banana peels like shoes?",#5
     "What do you call a shy lamb?",#6
+    "Why did cavemen draw pictures of hippopotamuses and rhinoceroses on their wall?",#7
+    "On what nuts can pictures hang on?",#8
+    "What do you call a bear with no teeth?",#9
+    "What happens when 500 hares get loose in the center of town?",#10
+    "What do you call a very popular perfume?",#11
+    "What happens when a ghost gets lost in the fog?",#12
+    "What do you call two spiders that just got married?",#13
+    "What paces back and forth on the ocean floor?",#14
+    "What goes thump, thump, thump, squish, thump, thump, thump, squish?",#15
+    "When is a baseball player like a spider?",#16
+    "What kind of a fish goes best with peanut butter?"#17
 ]
 Ajokes = [
     "Because he was outstanding in his field!",#1
@@ -44,6 +43,17 @@ Ajokes = [
     "A real tongue twister!",#4
     "They're pair of slippers.",#5
     "Baaash-ful",#6
+    "Because they couldn't spell the animals' names.",#7
+    "Wall-nuts.",#8
+    "A gummy bear!",#9
+    "The police had to comb the area.",#10
+    "A best-smeller.",#11
+    "He is mist.",#12
+    "Newlywebs.",#13
+    "A nervous wreck.",#14
+    "An elephant with one wet shoe.",#15
+    "When he catches a fly.",#16
+    "Jelly-fish.",#17
 ]
 
 
